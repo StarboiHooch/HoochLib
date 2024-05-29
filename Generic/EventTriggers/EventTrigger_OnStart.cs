96096dddaa629bcf0e6a8212c33d9ac286329f7c
hirokironimo <henrylwilkinson@gmail.com>
2024-05-29 12:07:42 +0100

    Rhythm Game basics working

---
+++ Generic/EventTriggers/EventTrigger_OnStart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HoochLib.Generic
+{
+    public class EventTrigger_OnStart : EventTrigger
+    {
+        private void Start()
+        {
+            InvokeEvents();
+        }
+    }
+}
